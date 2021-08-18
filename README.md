4ea87c9d77058239de3d2af893d6a1e2c4f72243
Miraculous Owonubi <omiraculous@gmail.com>
2021-08-18 12:05:10 +0100

    init near-api

---
+++ README.md
@@ -0,0 +1,65 @@
+# near-api-rs (N-AR)
+
+Rust crate for interacting with the NEAR Protocol via RPC API
+
+## Usage
+
+- Using a helper function:
+
+  ```rust
+  use near_api_rs::JsonRpcClient;
+
+  // this creates a new client internally, only use this if you only need one client
+  // see example 2 in the case where you need multiple clients
+  let rpc_client = JsonRpcClient::new_client("http://localhost:3030");
+
+  // The convenience methods on JsonRpcClient aid simplicity
+  let status = rpc_client.status().await;
+
+  println!("{:?}", status1);
+  ```
+
+- More involved syntax, decoupling method construction and execution and allowing method reuse
+
+  ```rust
+  // Here, we manually construct a method and execute that on a client
+  // This is useful if you have multiple clients to execute instructions on
+
+  use reqwest::Client;
+
+  use near_api_rs::{RpcMethod, JsonRpcClient};
+  use near_primitives::types::AccountId;
+  use near_jsonrpc_primitives::views::FinalExecutionOutcomeView;
+
+  let client = reqwest::Client::new(); // instantiate once, share everywhere
+
+  let rpc_client_1 = JsonRpcClient::new("http://localhost:3030", &client);
+  let rpc_client_2 = JsonRpcClient::new("http://rpc.website.com", &client);
+
+  let method = RpcMethod::Tx { id: "miraclx.near".parse::<AccountId>.unwrap() };
+
+  let tx_status_1: FinalExecutionOutcomeView = method.call_on(&rpc_client_1).await;
+  let tx_status_2: FinalExecutionOutcomeView = method.call_on(&rpc_client_2).await;
+
+  println!("{:?}", tx_status_1);
+  println!("{:?}", tx_status_2);
+  ```
+
+## Testing
+
+- Ensure you have the Rust compiler and package manager installed <https://rustup.rs/>
+- Get and initialize the NEAR sandbox <https://github.com/near/sandbox>
+- Thereafter;
+
+  ```console
+  near-sandbox --home /tmp/near-sandbox init # this happens once ;-)
+  near-sandbox --home /tmp/near-sandbox run
+  ```
+
+- Execute the test
+
+  ```console
+  git clone https://github.com/near/near-api-rs
+  cd near-api-rs
+  cargo test -- --nocapture
+  ```
---
+++ src/lib.rs
@@ -0,0 +1,321 @@
+use reqwest::Client;
+use serde::de::DeserializeOwned;
+use serde::Serialize;
+use serde_json::json;
+
+use near_jsonrpc_primitives::errors::RpcError;
+use near_jsonrpc_primitives::message::{from_slice, Message};
+use near_primitives::hash::CryptoHash;
+use near_primitives::types::{AccountId, BlockId, BlockReference, MaybeBlockId, ShardId};
+use near_primitives::views;
+
+#[derive(Debug, Serialize)]
+pub enum ChunkId {
+    BlockShardId(BlockId, ShardId),
+    Hash(CryptoHash),
+}
+
+pub enum ExperimentalRpcMethod {
+    CheckTx {
+        tx: String,
+    },
+    GenesisConfig,
+    BroadcastTxSync {
+        tx: String,
+    },
+    TxStatus {
+        tx: String,
+    },
+    Changes {
+        request: near_jsonrpc_primitives::types::changes::RpcStateChangesRequest,
+    },
+    ValidatorsOrdered {
+        request: near_jsonrpc_primitives::types::validator::RpcValidatorsOrderedRequest,
+    },
+    Receipt {
+        request: near_jsonrpc_primitives::types::receipts::RpcReceiptRequest,
+    },
+    ProtocolConfig {
+        request: near_jsonrpc_primitives::types::config::RpcProtocolConfigRequest,
+    },
+}
+
+pub enum RpcMethod {
+    BroadcastTxAsync {
+        tx: String,
+    },
+    BroadcastTxCommit {
+        tx: String,
+    },
+    Status,
+    Health,
+    Tx {
+        id: AccountId,
+    },
+    Chunk {
+        id: ChunkId,
+    },
+    Validators {
+        block_id: MaybeBlockId,
+    },
+    GasPrice {
+        block_id: MaybeBlockId,
+    },
+    QueryByPath {
+        path: String,
+        data: String,
+    },
+    Query {
+        request: near_jsonrpc_primitives::types::query::RpcQueryRequest,
+    },
+    BlockById {
+        block_id: BlockId,
+    },
+    Block {
+        request: BlockReference,
+    },
+    Experimental(ExperimentalRpcMethod),
+}
+
+impl RpcMethod {
+    fn method_and_params(&self) -> (&str, serde_json::Value) {
+        match self {
+            Self::BroadcastTxAsync { tx } => ("broadcast_tx_async", json!([tx])),
+            Self::BroadcastTxCommit { tx } => ("broadcast_tx_commit", json!([tx])),
+            Self::Status => ("status", json!([])),
+            Self::Health => ("health", json!([])),
+            Self::Tx { id } => ("tx", json!([id])),
+            Self::Chunk { id } => ("chunk", json!([id])),
+            Self::Validators { block_id } => ("validators", json!([block_id])),
+            Self::GasPrice { block_id } => ("gas_price", json!([block_id])),
+            Self::QueryByPath { path, data } => ("query", json!([path, data])),
+            Self::Query { request } => ("query", json!(request)),
+            Self::BlockById { block_id } => ("block", json!([block_id])),
+            Self::Block { request } => ("block", json!(request)),
+            Self::Experimental(ExperimentalRpcMethod::CheckTx { tx }) => {
+                ("EXPERIMENTAL_check_tx", json!([tx]))
+            }
+            Self::Experimental(ExperimentalRpcMethod::GenesisConfig) => {
+                ("EXPERIMENTAL_genesis_config", json!([]))
+            }
+            Self::Experimental(ExperimentalRpcMethod::BroadcastTxSync { tx }) => {
+                ("EXPERIMENTAL_broadcast_tx_sync", json!([tx]))
+            }
+            Self::Experimental(ExperimentalRpcMethod::TxStatus { tx }) => {
+                ("EXPERIMENTAL_tx_status", json!([tx]))
+            }
+            Self::Experimental(ExperimentalRpcMethod::Changes { request }) => {
+                ("EXPERIMENTAL_changes", json!(request))
+            }
+            Self::Experimental(ExperimentalRpcMethod::ValidatorsOrdered { request }) => {
+                ("EXPERIMENTAL_validators_ordered", json!(request))
+            }
+            Self::Experimental(ExperimentalRpcMethod::Receipt { request }) => {
+                ("EXPERIMENTAL_receipt", json!(request))
+            }
+            Self::Experimental(ExperimentalRpcMethod::ProtocolConfig { request }) => {
+                ("EXPERIMENTAL_protocol_config", json!(request))
+            }
+        }
+    }
+
+    pub async fn call_on<T: DeserializeOwned>(
+        &self,
+        rpc_client: &JsonRpcClient,
+    ) -> Result<T, RpcError> {
+        let (method_name, params) = self.method_and_params();
+        let request_payload = Message::request(method_name.to_string(), Some(params));
+        let request = rpc_client
+            .client
+            .post(&rpc_client.server_addr)
+            .header("Content-Type", "application/json")
+            .json(&request_payload);
+        let response = request
+            .send()
+            .await
+            .map_err(|err| RpcError::new_internal_error(None, format!("{:?}", err)))?;
+        let response_payload = response.bytes().await.map_err(|err| {
+            RpcError::parse_error(format!("Failed to retrieve response payload: {:?}", err))
+        })?;
+        if let Message::Response(response) = from_slice(&response_payload).map_err(|err| {
+            RpcError::parse_error(format!("Failed parsing response payload: {:?}", err))
+        })? {
+            return serde_json::from_value(response.result?)
+                .map_err(|err| RpcError::parse_error(format!("Failed to parse: {:?}", err)));
+        }
+        Err(RpcError::parse_error(format!(
+            "Failed to parse JSON RPC response"
+        )))
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct JsonRpcClient {
+    server_addr: String,
+    client: Client,
+}
+
+impl JsonRpcClient {
+    pub fn new(server_addr: &str, client: &Client) -> Self {
+        Self {
+            server_addr: server_addr.to_string(),
+            client: client.clone(),
+        }
+    }
+
+    pub fn new_client(server_addr: &str) -> Self {
+        Self {
+            server_addr: server_addr.to_string(),
+            client: Client::new(),
+        }
+    }
+
+    pub async fn broadcast_tx_async(&self, tx: String) -> Result<String, RpcError> {
+        RpcMethod::BroadcastTxAsync { tx }.call_on(self).await
+    }
+
+    pub async fn broadcast_tx_commit(
+        &self,
+        tx: String,
+    ) -> Result<views::FinalExecutionOutcomeView, RpcError> {
+        RpcMethod::BroadcastTxCommit { tx }.call_on(self).await
+    }
+
+    pub async fn status(&self) -> Result<views::StatusResponse, RpcError> {
+        RpcMethod::Status.call_on(self).await
+    }
+
+    pub async fn health(&self) -> Result<(), RpcError> {
+        RpcMethod::Health.call_on(self).await
+    }
+
+    pub async fn tx(&self, id: AccountId) -> Result<views::FinalExecutionOutcomeView, RpcError> {
+        RpcMethod::Tx { id }.call_on(self).await
+    }
+
+    pub async fn chunk(&self, id: ChunkId) -> Result<views::ChunkView, RpcError> {
+        RpcMethod::Chunk { id }.call_on(self).await
+    }
+
+    pub async fn validators(
+        &self,
+        block_id: MaybeBlockId,
+    ) -> Result<views::EpochValidatorInfo, RpcError> {
+        RpcMethod::Validators { block_id }.call_on(self).await
+    }
+
+    pub async fn gas_price(&self, block_id: MaybeBlockId) -> Result<views::GasPriceView, RpcError> {
+        RpcMethod::GasPrice { block_id }.call_on(self).await
+    }
+
+    pub async fn query_by_path(
+        &self,
+        path: String,
+        data: String,
+    ) -> Result<near_jsonrpc_primitives::types::query::RpcQueryResponse, RpcError> {
+        RpcMethod::QueryByPath { path, data }.call_on(self).await
+    }
+
+    pub async fn query(
+        &self,
+        request: near_jsonrpc_primitives::types::query::RpcQueryRequest,
+    ) -> Result<near_jsonrpc_primitives::types::query::RpcQueryResponse, RpcError> {
+        RpcMethod::Query { request }.call_on(self).await
+    }
+
+    pub async fn block_by_id(&self, block_id: BlockId) -> Result<views::BlockView, RpcError> {
+        RpcMethod::BlockById { block_id }.call_on(self).await
+    }
+
+    pub async fn block(&self, request: BlockReference) -> Result<views::BlockView, RpcError> {
+        RpcMethod::Block { request }.call_on(self).await
+    }
+
+    #[allow(non_snake_case)]
+    pub async fn EXPERIMENTAL_check_tx(&self, tx: String) -> Result<serde_json::Value, RpcError> {
+        RpcMethod::Experimental(ExperimentalRpcMethod::CheckTx { tx })
+            .call_on(self)
+            .await
+    }
+
+    #[allow(non_snake_case)]
+    pub async fn EXPERIMENTAL_genesis_config(&self) -> Result<serde_json::Value, RpcError> {
+        RpcMethod::Experimental(ExperimentalRpcMethod::GenesisConfig)
+            .call_on(self)
+            .await
+    }
+
+    #[allow(non_snake_case)]
+    pub async fn EXPERIMENTAL_broadcast_tx_sync(
+        &self,
+        tx: String,
+    ) -> Result<serde_json::Value, RpcError> {
+        RpcMethod::Experimental(ExperimentalRpcMethod::BroadcastTxSync { tx })
+            .call_on(self)
+            .await
+    }
+
+    #[allow(non_snake_case)]
+    pub async fn EXPERIMENTAL_tx_status(&self, tx: String) -> Result<serde_json::Value, RpcError> {
+        RpcMethod::Experimental(ExperimentalRpcMethod::TxStatus { tx })
+            .call_on(self)
+            .await
+    }
+
+    #[allow(non_snake_case)]
+    pub async fn EXPERIMENTAL_changes(
+        &self,
+        request: near_jsonrpc_primitives::types::changes::RpcStateChangesRequest,
+    ) -> Result<near_jsonrpc_primitives::types::changes::RpcStateChangesResponse, RpcError> {
+        RpcMethod::Experimental(ExperimentalRpcMethod::Changes { request })
+            .call_on(self)
+            .await
+    }
+
+    #[allow(non_snake_case)]
+    pub async fn EXPERIMENTAL_validators_ordered(
+        &self,
+        request: near_jsonrpc_primitives::types::validator::RpcValidatorsOrderedRequest,
+    ) -> Result<Vec<views::validator_stake_view::ValidatorStakeView>, RpcError> {
+        RpcMethod::Experimental(ExperimentalRpcMethod::ValidatorsOrdered { request })
+            .call_on(self)
+            .await
+    }
+
+    #[allow(non_snake_case)]
+    pub async fn EXPERIMENTAL_receipt(
+        &self,
+        request: near_jsonrpc_primitives::types::receipts::RpcReceiptRequest,
+    ) -> Result<near_jsonrpc_primitives::types::receipts::RpcReceiptResponse, RpcError> {
+        RpcMethod::Experimental(ExperimentalRpcMethod::Receipt { request })
+            .call_on(self)
+            .await
+    }
+
+    #[allow(non_snake_case)]
+    pub async fn EXPERIMENTAL_protocol_config(
+        &self,
+        request: near_jsonrpc_primitives::types::config::RpcProtocolConfigRequest,
+    ) -> Result<near_jsonrpc_primitives::types::config::RpcProtocolConfigResponse, RpcError> {
+        RpcMethod::Experimental(ExperimentalRpcMethod::ProtocolConfig { request })
+            .call_on(self)
+            .await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{JsonRpcClient, RpcMethod};
+
+    #[tokio::test]
+    async fn it_works() {
+        let rpc_client = JsonRpcClient::new_client("http://localhost:3030");
+        let status1 = rpc_client.status().await;
+        let status2 = RpcMethod::Status
+            .call_on::<near_primitives::views::StatusResponse>(&rpc_client)
+            .await;
+
+        println!("{:?}", status1);
+        println!("{:?}", status2);
+    }
+}
