9c26f4acb8bd73fd77bdfd449005d4f150e62455
Miraculous Owonubi <omiraculous@gmail.com>
2021-12-22 02:39:30 +0100

    add view account example

---
+++ examples/view_account.rs
@@ -0,0 +1,24 @@
+use near_jsonrpc_client::{methods, JsonRpcClient};
+use near_jsonrpc_primitives::types::query::QueryResponseKind;
+use near_primitives::types::{BlockReference, Finality};
+use near_primitives::views::QueryRequest;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = JsonRpcClient::connect("https://rpc.mainnet.near.org");
+
+    let request = methods::query::RpcQueryRequest {
+        block_reference: BlockReference::Finality(Finality::Final),
+        request: QueryRequest::ViewAccount {
+            account_id: "miraclx.near".parse()?,
+        },
+    };
+
+    let response = client.call(request).await?;
+
+    if let QueryResponseKind::ViewAccount(result) = response.kind {
+        println!("{:#?}", result);
+    }
+
+    Ok(())
+}
