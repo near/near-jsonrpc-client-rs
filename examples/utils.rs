b130118d0de806bd9950be306f563559f07c77e6
Miraculous Owonubi <omiraculous@gmail.com>
2021-12-23 13:43:33 +0100

    examples: support custom rpc addr

--- examples/utils.rs
+++ examples/utils.rs
@@ -30,37 +30,50 @@ pub fn select_network() -> io::Result<JsonRpcClient<auth::Unauthenticated>> {
         || {
             println!(" [1] mainnet \x1b[38;5;244m(alias: m, main)\x1b[0m");
             println!(" [2] testnet \x1b[38;5;244m(alias: t, test)\x1b[0m");
+            println!(" [3] custom  \x1b[38;5;244m(alias:       c)\x1b[0m");
         },
         "\x1b[33m(enter a selection)\x1b[0m> ",
         |selection| match (selection, selection.parse()) {
             ("m" | "main" | "mainnet", _) | (_, Ok(1)) => Some("mainnet"),
             ("t" | "test" | "testnet", _) | (_, Ok(2)) => Some("testnet"),
+            ("c" | "custom", _) | (_, Ok(2)) => Some("custom"),
             _ => None,
         },
     )?;
-    let archival = select(
-        || (),
-        "Should we connect to an archival node? [y/N] ",
-        |selection| match selection {
-            "" | "n" | "no" => Some(false),
-            "y" | "yes" => Some(true),
-            _ => None,
-        },
-    )?;
-    println!(
-        "\x1b[32m(i)\x1b[0m Connected to the [{}] network{}",
-        network,
-        if archival {
-            " (via an archival node)"
-        } else {
-            ""
+    let network_url = if network != "custom" {
+        let archival = select(
+            || (),
+            "Should we connect to an archival node? [y/N] ",
+            |selection| match selection {
+                "" | "n" | "no" => Some(false),
+                "y" | "yes" => Some(true),
+                _ => None,
+            },
+        )?;
+        println!(
+            "\x1b[32m(i)\x1b[0m Connected to the [{}] network{}",
+            network,
+            if archival {
+                " (via an archival node)"
+            } else {
+                ""
+            }
+        );
+        format!(
+            "https://{archival}rpc.{network}.near.org",
+            archival = if archival { "archival-" } else { "" },
+            network = network
+        )
+    } else {
+        loop {
+            let url = input("Enter the RPC Server Address: ")?;
+            if let Err(err) = url.parse::<reqwest::Url>() {
+                println!("\x1b[31m(i)\x1b[0m invalid url ({}), retry..", err);
+                continue;
+            }
+            break url;
         }
-    );
-    let network_url = format!(
-        "https://{archival}rpc.{network}.near.org",
-        archival = if archival { "archival-" } else { "" },
-        network = network
-    );
+    };
     println!("===================================");
 
     Ok(JsonRpcClient::connect(&network_url))
