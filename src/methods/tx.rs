cb1552fec7fe8890e70777e6440b04bf13ae27b6
Femi Bankole <llordbanks500@gmail.com>
2022-03-09 20:40:50 +0100

    chore(docs): document `tx` jsonrpc method (#53)

--- src/methods/tx.rs
+++ src/methods/tx.rs
@@ -1,3 +1,30 @@
+//! Queries the status of a transaction.
+//!
+//! ## Example
+//! Returns the final transaction result for
+//! <https://explorer.near.org/transactions/B9aypWiMuiWR5kqzewL9eC96uZWA3qCMhLe67eBMWacq>
+//!
+//! ```
+//! use near_jsonrpc_client::{methods, JsonRpcClient};
+//!
+//! # #[tokio::main]
+//! # async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//! let client = JsonRpcClient::connect("https://archival-rpc.mainnet.near.org");
+//! let tx_hash = "B9aypWiMuiWR5kqzewL9eC96uZWA3qCMhLe67eBMWacq".parse()?;
+//!
+//! let request = methods::tx::RpcTransactionStatusRequest {
+//!     transaction_info: methods::tx::TransactionInfo::TransactionId {
+//!         hash: tx_hash,
+//!         account_id: "itranscend.near".parse()?,
+//!    }
+//! };
+//!
+//! let response = client.call(request).await?;
+//!
+//! assert_eq!(tx_hash, response.transaction.hash);
+//! # Ok(())
+//! # }
+//! ```
 use super::*;
 
 pub use near_jsonrpc_primitives::types::transactions::RpcTransactionError;
