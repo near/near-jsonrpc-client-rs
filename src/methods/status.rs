83fdba146041551e7d60c5a16ad0e0e22c6e47ff
Femi Bankole <femibankole70@gmail.com>
2022-03-11 14:57:44 +0100

    chore(docs): document `status` jsonrpc method

--- src/methods/status.rs
+++ src/methods/status.rs
@@ -1,3 +1,28 @@
+//! Queries the status of a given node
+//!
+//! Returns the general status of a given node (sync status, nearcore node version, protocol version, etc), and the current set of validators.
+//!
+//! ## Example
+//! ```
+//! use near_jsonrpc_client::{methods, JsonRpcClient};
+//!
+//! # #[tokio::main]
+//! # async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//! let client = JsonRpcClient::connect("https://rpc.testnet.near.org");
+//!
+//! let request = methods::status::RpcStatusRequest;
+//!
+//! let response = client.call(request).await;
+//!
+//! assert!(
+//!     matches!(response, Ok(methods::status::RpcStatusResponse { .. })),
+//!     "expected an Ok(RpcStatusResponse) but got [{:?}]",
+//!     response
+//! );
+//!
+//! # Ok(())
+//! # }
+//! ```
 use super::*;
 
 pub use near_jsonrpc_primitives::types::status::RpcStatusError;
