3917c5b9e2561c4b40e27c865ad37bb78a761a3a
iTranscend <llordbanks500@gmail.com>
2022-03-12 05:04:04 +0100

    chore(docs): document the `network_info` RPC method

--- src/methods/network_info.rs
+++ src/methods/network_info.rs
@@ -1,3 +1,28 @@
+//! Requests information about nodes on the network.
+//!
+//! Returns the current state of node network connections (active peers, transmitted data, received data, known producers, etc.).
+//!
+//! ## Example
+//!
+//! ```
+//! use near_jsonrpc_client::{methods, JsonRpcClient};
+//!
+//! # #[tokio::main]
+//! # async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//! let client = JsonRpcClient::connect("https://rpc.testnet.near.org");
+//!
+//! let request = methods::network_info::RpcNetworkInfoRequest;
+//!
+//! let response = client.call(request).await;
+//!
+//! assert!(matches!(
+//!     response,
+//!     Ok(methods::network_info::RpcNetworkInfoResponse { .. })
+//! ));
+//!
+//! # Ok(())
+//! # }
+//! ```
 use super::*;
 
 pub use near_jsonrpc_primitives::types::network_info::{
