c77f5ba3acaf9f67616ceef951b9083ae82ba204
iTranscend <llordbanks500@gmail.com>
2022-03-13 14:39:47 +0100

    chore(docs): document the `health` RPC method

--- src/methods/health.rs
+++ src/methods/health.rs
@@ -1,3 +1,27 @@
+//! Requests health status of the rpc node.
+//!
+//! ## Example
+//!
+//! Returns the current health stauts of the rpc node it connects to.
+//!
+//! ```
+//! use near_jsonrpc_client::{methods, JsonRpcClient};
+//!
+//! # #[tokio::main]
+//! # async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//! let client = JsonRpcClient::connect("https://rpc.testnet.near.org");
+//!
+//! let request = methods::health::RpcHealthRequest;
+//!
+//! let response = client.call(request).await?;
+//!
+//! assert!(matches!(
+//!     response,
+//!     methods::health::RpcHealthResponse { .. }
+//! ));
+//! # Ok(())
+//! # }
+//! ```
 use super::*;
 
 pub use near_jsonrpc_primitives::types::status::{RpcHealthResponse, RpcStatusError};
