b7a9412d84e3079b7f9a98906518756067f6d5ed
iTranscend <llordbanks500@gmail.com>
2022-04-10 06:15:42 +0100

    chore(docs): document the `light_client_proof` RPC method

--- src/methods/light_client_proof.rs
+++ src/methods/light_client_proof.rs
@@ -1,3 +1,30 @@
+//! Verifies whether a transaction happened on chain.
+//!
+//! ```
+//! use near_jsonrpc_client::{methods, JsonRpcClient};
+//! use near_primitives::types::TransactionOrReceiptId;
+//!
+//! # #[tokio::main]
+//! # async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//! let client = JsonRpcClient::connect("");
+//!
+//! let request = methods::light_client_proof::RpcLightClientExecutionProofRequest {
+//!     id: TransactionOrReceiptId::Transaction {
+//!             transaction_hash: "".parse()?,
+//!             sender_id: "".parse()?,
+//!         },
+//!     light_client_head: "".parse()?,
+//! };
+//!
+//! let response = client.call(request).await?;
+//!
+//! assert!(matches!(
+//!     response,
+//!     methods::light_client_proof::RpcLightClientExecutionProofResponse { .. }
+//! ));
+//! Ok(())
+//! # }
+//! ```
 use super::*;
 
 pub use near_jsonrpc_primitives::types::light_client::{
