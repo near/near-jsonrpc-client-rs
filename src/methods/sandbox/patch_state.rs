44e15ef2a9abceb1711efb82344349df6e4cb8b0
iTranscend <llordbanks500@gmail.com>
2022-04-13 08:15:16 +0100

    chore(docs): document the `sandbox_patch_state` RPC method

--- src/methods/sandbox/patch_state.rs
+++ src/methods/sandbox/patch_state.rs
@@ -1,3 +1,38 @@
+//! Patch account, access keys, contract code, or contract state.
+//!
+//! Only additions and mutations are supported. No deletions. 
+//! 
+//! Account, access keys, contract code, and contract states have different formats. See the example and docs for details about their format.
+//!
+//! ## Examples
+//!
+//! ```
+//! use near_jsonrpc_client::{methods, JsonRpcClient};
+//! use near_primitives::{state_record::StateRecord, account, types::{AccountId, StorageUsage}};
+//! use near_primitives::{account::AccountVersion, hash::CryptoHash};
+//!
+//! # #[tokio::main]
+//! # async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//! let client = JsonRpcClient::connect("http://localhost:3030");
+//!
+//! let request = methods::sandbox_patch_state::RpcSandboxPatchStateRequest {
+//!     records: vec![
+//!         StateRecord::Account {
+//!             account_id: "fido.testnet".parse::<AccountId>()?,
+//!             account: account::Account::new(179, 0, CryptoHash::default(), 264)
+//!          }
+//!     ],
+//! };
+//!
+//! let response = client.call(request).await?;
+//!
+//! assert!(matches!(
+//!     response,
+//!     methods::sandbox_patch_state::RpcSandboxPatchStateResponse { .. }
+//! ));
+//! # Ok(())
+//! # }
+//! ```
 use super::*;
 
 pub use near_jsonrpc_primitives::types::sandbox::{
