644347133be4b0ee76edd75c5ebe258e4916bea6
iTranscend <llordbanks500@gmail.com>
2022-04-19 21:29:27 +0100

    chore(docs): document the `auth` module

--- src/auth.rs
+++ src/auth.rs
@@ -1,3 +1,33 @@
+//! Client Authentication.
+//! 
+//! Some RPC nodes will require authentication before requests can be sent to them. 
+//! 
+//! This is done by adding your api key to the request as a header.
+//! 
+//! ## Example
+//! 
+//! ```
+//! use near_jsonrpc_client::{JsonRpcClient, auth, methods};
+//! use near_primitives::types::{BlockReference, Finality};
+//! 
+//! # #[tokio::main]
+//! # async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//! let client = JsonRpcClient::connect("https://testnet.rpc.near.dev/")
+//!     .header(auth::ApiKey::new("399ba741-e939-4ffa-8c3c-306ec36fa8de")?);
+//! 
+//! let request = methods::block::RpcBlockRequest {
+//!     block_reference: BlockReference::Finality(Finality::Final),
+//! };
+//! 
+//! let response = client.call(request).await?;
+//! 
+//! assert!(matches!(
+//!     response,
+//!     methods::block::RpcBlockResponse { .. }
+//! ));
+//! # Ok(())
+//! # }
+//! ```
 use std::fmt;
 
 use reqwest::header::HeaderValue;
