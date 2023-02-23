edec8ab6e6d86e2b4c62b123d9cb1176ecbbcde5
anthony-near <113615218+anthony-near@users.noreply.github.com>
2023-02-23 14:55:32 -0600

    Update src/methods/mod.rs
    simplified
    
    Co-authored-by: Miraculous Owonubi <omiraculous@gmail.com>

--- src/methods/mod.rs
+++ src/methods/mod.rs
@@ -145,7 +145,7 @@ pub use adversarial::adv_check_store;
 pub fn to_json<M: RpcMethod>(method: &M) -> Result<serde_json::Value, io::Error> {
     let request_payload = near_jsonrpc_primitives::message::Message::request(
         method.method_name().to_string(),
-        Option::from(method.params().unwrap()).into(),
+        method.params()?,
     );
 
     Ok(json!(request_payload))
